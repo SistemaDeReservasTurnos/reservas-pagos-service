69fb44d3a8024d443ca0243aeb82b6f430f10c3d
Samuel <escobar.samuel@correounivalle.edu.co>
2025-11-14 13:52:13 -0500

    Se crea la estructura base del poryecto

---
+++ reservas-pagos-service/src/test/java/com/servicio/reservas/pago/ReservasPagosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.reservas.pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasPagosServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
